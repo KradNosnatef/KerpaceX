a53f11e3869a5618e7d1f3cf0445361c1206e499
JeffreyXiang <junx93002@sina.com>
2020-01-15 17:05:36 +0800

    VerticalVelocityKeeping.java added

--- KerpaceX_Project/src/controller/VerticalVelocityKeeping.java
+++ KerpaceX_Project/src/controller/VerticalVelocityKeeping.java
@@ -1,26 +1,33 @@
+/*
+ * VerticalVelocityKeeping.java
+ * Author: Jeffrey Xiang
+ * Data: 2020.1.15
+ */
 import java.util.TimerTask;
 
 import krpc.client.RPCException;
 import krpc.client.services.SpaceCenter;
 import krpc.client.services.SpaceCenter.ReferenceFrame;
 
+//垂直速度保持模块，以50Hz运行
 public class VerticalVelocityKeeping extends TimerTask
 {
-	private SpaceCenter.Vessel vessel;
-	private SpaceCenter.Flight flight;
-	private double target;
-	private double current;
-	private boolean isRunning;
-	private float throttle = 0;
-	private float thrustWeightRatio;
-	private int count;
-	private double Kp = 0.1;
-	private double Ki = 1e-2;
-	private double Kd = -0.5;
-	private double Dp;
-	private double Di = 0;
-	private double Dd;
+	private SpaceCenter.Vessel vessel;	//飞船对象
+	private SpaceCenter.Flight flight;	//飞行对象，必须以Kerbin参考系建立
+	private double target;				//目标垂直速度
+	private double current;				//当前垂直速度
+	private boolean isRunning;			//是否在运行
+	private float throttle = 0;			//节流阀大小
+	private float thrustWeightRatio;	//推重比
+	private int count;					//计数，用于每10次计算一次推重比（每次都算太慢）
+	private double Kp = 0.1;			//比例参数
+	private double Ki = 1e-2;			//积分参数
+	private double Kd = -0.5;			//微分参数
+	private double Dp;					//比例偏差
+	private double Di = 0;				//积分偏差
+	private double Dd;					//微分偏差
 	
+	//构造函数，传入目标飞船，与Kerbin参考系
 	public VerticalVelocityKeeping(SpaceCenter.Vessel vessel,ReferenceFrame refFrame) throws RPCException
 	{
 		this.vessel = vessel;
@@ -29,11 +36,13 @@ public class VerticalVelocityKeeping extends TimerTask
 		isRunning = false;
 	}
 	
+	//设置目标垂直速度
 	public void setTarget(double target)
 	{
 		this.target = target;
 	}
 	
+	//设置PID参数
 	public void setParameter(double Kp, double Ki, double Kd)
 	{
 		this.Kp = Kp;
@@ -41,6 +50,7 @@ public class VerticalVelocityKeeping extends TimerTask
 		this.Kd = Kd;
 	}
 	
+	//运行一次（若需以Runnable运行，请整体包while(true)）
 	public void run()
 	{
 		try
@@ -71,12 +81,14 @@ public class VerticalVelocityKeeping extends TimerTask
 		}
 	}
 	
+	//启动保持
 	public void start()
 	{
 		isRunning = true;
 		count = 0;
 	}
 	
+	//终止保持
 	public void stop()
 	{
 		isRunning = false;
