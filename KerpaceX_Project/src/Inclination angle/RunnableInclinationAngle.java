817c1334dbc1cc068fcf38515a7c6d01afeb4792
ang <17bbayukari@gmail.com>
2020-01-20 01:47:28 +0800

    计算当前倾斜角，但测试部分不完善

---
+++ KerpaceX_Project/src/Inclination angle/RunnableInclinationAngle.java
@@ -0,0 +1,63 @@
+package inclinationAngle;
+
+import krpc.client.RPCException;
+import krpc.client.services.SpaceCenter.CelestialBody;
+import krpc.client.services.SpaceCenter.Vessel;
+
+public class RunnableInclinationAngle implements Runnable {
+	static final int ap = 80000; // the target apogee is 80km
+	private Thread t = null;
+	private Vessel vessel = null;
+	private CelestialBody celestialBody = null;
+	private double inclinationAngle = 0;
+
+	//Calculate the gravity of the spacecraft
+	private float currentGravity(float mass,float distance,float gravitationalParameter){
+		return mass*gravitationalParameter/distance/distance;
+	}
+	public double getInclinationAngle(){
+		return inclinationAngle;
+	}
+	//"stop signal" callback function
+	void signal() {
+
+	}
+
+	RunnableInclinationAngle(Vessel vessel,CelestialBody celestialBody) {
+		this.vessel = vessel;
+		this.celestialBody = celestialBody;
+	}
+
+
+
+	@Override
+	public void run() {
+		float gravity = 0;
+		while (true) {
+			try {
+				Thread.sleep(10);
+
+				if (vessel.flight(null).getMeanAltitude() >= ap) {
+					signal();
+				}
+				
+				gravity = currentGravity(vessel.getMass(),(float)(vessel.flight(null).getMeanAltitude()+celestialBody.getEquatorialRadius()),celestialBody.getGravitationalParameter());
+				
+				inclinationAngle = Math.asin(gravity/vessel.getAvailableThrust());
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	public void start(){
+		if(t==null){
+			t = new Thread(this,"Angel");
+			t.start();
+		}
+	}
+}
---
+++ KerpaceX_Project/src/Inclination angle/testBench/RunnableInclinationAngle_TB.java
@@ -0,0 +1,24 @@
+package inclinationAngle;
+
+import java.io.IOException;
+
+import krpc.client.Connection;
+import krpc.client.RPCException;
+import krpc.client.services.SpaceCenter;
+
+public class RunnableInclinationAngle_TB {
+    public static void main(String[] args) throws IOException, RPCException, InterruptedException {
+        Connection connection = Connection.newInstance("Launch into orbit");
+        SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
+        SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
+        SpaceCenter.CelestialBody celestialBody = spaceCenter.getTargetBody();
+
+        RunnableInclinationAngle runnableInclinationAngle = new RunnableInclinationAngle(vessel, celestialBody);
+        runnableInclinationAngle.start();
+
+		while(true){
+            System.out.println(runnableInclinationAngle.getInclinationAngle());
+			Thread.sleep(1000);
+		}
+}
+}
