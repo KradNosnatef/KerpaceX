8d65eadbdbecbff4e51f2d5ddb5264f5a0349cb1
KradNosnatef <fuqianshan@foxmail.com>
2020-01-20 10:50:18 +0800

    滑稽一刻

--- KerpaceX_Project/src/controller/ImpactPos.java
+++ KerpaceX_Project/src/controller/ImpactPos.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import core.KosKrpcJSON;
 
 public class ImpactPos {//在实例化之后你可以refreshImpactPos以更新预计撞击点经纬度数据,然后用两个get方法获取它们
-	GeoCoordinates geoCoordinates;
+	GeoCoordinates impactPosGeoCoordinates;
 	KosKrpcJSON jsonConnection;
 	public ImpactPos() {
 		jsonConnection=new KosKrpcJSON("Ships\\Script\\TRdata.json",GeoCoordinates.class);
 	}
 	public void refreshImpactPos() throws IOException {//注意:这个程序的有效执行频率上限是10HZ,超过10HZ的部分是无效的
-		geoCoordinates=(GeoCoordinates) jsonConnection.getObject();//JSON通信!
-		System.out.println(geoCoordinates.lat+" "+geoCoordinates.lng);//打log,可以注释掉
+		impactPosGeoCoordinates=(GeoCoordinates) jsonConnection.getObject();//JSON通信!
+		System.out.println(impactPosGeoCoordinates.lat+" "+impactPosGeoCoordinates.lng);//打log,可以注释掉
 	}
 	public double getImpactPosLat() {
-		return geoCoordinates.lat;
+		return impactPosGeoCoordinates.lat;
 	}
 	public double getImpactPosLng() {
-		return geoCoordinates.lng;
+		return impactPosGeoCoordinates.lng;
 	}
 }
