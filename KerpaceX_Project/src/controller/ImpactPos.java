f7ec1b39fb4d70e0e82eed448f9117f0b7696401
KradNosnatef <fuqianshan@foxmail.com>
2020-01-21 11:26:33 +0800

    去掉log

--- KerpaceX_Project/src/controller/ImpactPos.java
+++ KerpaceX_Project/src/controller/ImpactPos.java
@@ -12,7 +12,7 @@ public class ImpactPos {//在实例化之后你可以refreshImpactPos以更新预计撞击点经纬
 	}
 	public void refreshImpactPos() throws IOException {//注意:这个程序的有效执行频率上限是10HZ,超过10HZ的部分是无效的
 		impactPosGeoCoordinates=(GeoCoordinates) jsonConnection.getObject();//JSON通信!
-		System.out.println(impactPosGeoCoordinates.lat+" "+impactPosGeoCoordinates.lng);//打log,可以注释掉
+		//System.out.println(impactPosGeoCoordinates.lat+" "+impactPosGeoCoordinates.lng);//打log,可以注释掉
 	}
 	public double getImpactPosLat() {
 		return impactPosGeoCoordinates.lat;
