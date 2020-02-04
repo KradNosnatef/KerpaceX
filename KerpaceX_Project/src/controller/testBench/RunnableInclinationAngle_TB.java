ad083eb379474773896412ea08ce2d17e759cf21
KradNosnatef <fuqianshan@foxmail.com>
2020-02-04 19:21:33 +0800

    补充上一个更改

--- KerpaceX_Project/src/controller/testBench/RunnableInclinationAngle_TB.java
+++ KerpaceX_Project/src/controller/testBench/RunnableInclinationAngle_TB.java
@@ -1,8 +1,11 @@
-package caculator.testBench;
+package controller.testBench;
 
 import java.io.IOException;
 
+import org.javatuples.Triplet;
+
 import caculator.RunnableInclinationAngle;
+import controller.PropulsionSystem;
 import krpc.client.Connection;
 import krpc.client.RPCException;
 import krpc.client.services.SpaceCenter;
@@ -14,14 +17,29 @@ public class RunnableInclinationAngle_TB {
         SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
         SpaceCenter.CelestialBody celestialBody = spaceCenter.getBodies().get("Kerbin");
 
+        PropulsionSystem firstStageEngine=new PropulsionSystem(vessel,"firstStage");
+        PropulsionSystem leftBoosterEngine=new PropulsionSystem(vessel,"leftBooster");
+        PropulsionSystem rightBoosterEngine=new PropulsionSystem(vessel,"rightBooster");
+        
         RunnableInclinationAngle runnableInclinationAngle = new RunnableInclinationAngle(vessel, celestialBody);
         runnableInclinationAngle.start();
 
         vessel.getControl().setSAS(false);
         vessel.getControl().setRCS(false);
+        
         vessel.getControl().setThrottle(1);
-        vessel.getControl().activateNextStage();
+        firstStageEngine.setAllEngineThrottle(1);
+        firstStageEngine.enableAllEngines();
+        leftBoosterEngine.setAllEngineThrottle(1);
+        leftBoosterEngine.enableAllEngines();
+        rightBoosterEngine.setAllEngineThrottle(1);
+        rightBoosterEngine.enableAllEngines();
+        
         vessel.getAutoPilot().engage();
+        Triplet<Double, Double, Double> value=new Triplet<Double, Double, Double>(1.0,1.0,1.0);
+        vessel.getAutoPilot().setStoppingTime(value);
+        vessel.getAutoPilot().setPitchPIDGains(value);
+        vessel.getAutoPilot().setYawPIDGains(value);
         vessel.getAutoPilot().targetPitchAndHeading(90, 90);
 		while(true){
 			if(runnableInclinationAngle.exit) break;
